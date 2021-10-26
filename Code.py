bf005537e92a238dba231c2e2ecad8a4f3c07afa
itsjotab <63935476+itsjotab@users.noreply.github.com>
2021-10-26 05:48:19 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,10 +2,9 @@ starting_principal = int(input("Enter Starting Amount: "))
 interest_rate = int(input("Interest amount: "))/100
 length = int(input("Total days of compound: "))
 nsp = starting_principal * (interest_rate + 1)
-start = 0
+start = 1
 
-while length > start:
-    print(length, round( nsp, 2))
-    length -= 1
-    nsp = nsp * (interest_rate + 1)
-#print("Your total", round(nsp, 2), "After", start, "Dates")    
+while length >= start:
+    print(start, round( nsp, 2))
+    start += 1
+    nsp = nsp * (interest_rate + 1)   
